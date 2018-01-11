719afc74aa8e1a61eda94ab4d792f8d72fe8bbd4
wbh <bhwang2004@gmail.com>
2018-01-11 16:09:47 +0800

    Minor update

--- GenericOrderAssignment/src/com/topology/f2p/Order.java
+++ GenericOrderAssignment/src/com/topology/f2p/Order.java
@@ -10,32 +10,11 @@ public class Order {
     ShipOption shipOption;
     double x;
     double y;
-//    int hour;
-//    int minute;
-
     OrderStatus status  = OrderStatus.UNFULFILLED;
 
     boolean prime;
-//    double score;
 
     LocalTime time;
-
-//    public Order(Map<ASIN, Integer> units, String id, ShipOption shipOption, double x, double y) {
-//        this.units = units;
-//        this.id = id;
-//        this.shipOption = shipOption;
-//        this.x = x;
-//        this.y = y;
-//    }
-
-//    public Order(Map<ASIN, Integer> units, String id, ShipOption shipOption, double x, double y, int hour) {
-//        this.units = units;
-//        this.id = id;
-//        this.shipOption = shipOption;
-//        this.x = x;
-//        this.y = y;
-//        this.hour = hour;
-//    }
 
     public Order(Map<ASIN, Integer> units, String id, ShipOption shipOption, double x, double y, LocalTime time) {
         this.units = units;
@@ -45,11 +24,4 @@ public class Order {
         this.y = y;
         this.time = time;
     }
-
-//    public String getTimeStr(){
-//        if(minute < 10)
-//            return hour + ":0" + minute;
-//        else
-//            return hour + ":" + minute;
-//    }
 }
